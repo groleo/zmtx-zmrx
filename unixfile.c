7728741d755b0e4647633e19e1504fbbfc0cc18b
Adrian Negreanu <adrian.negreanu@nxp.com>
2024-06-14 22:53:25 +0300

    unixfile: implement get_matching_files

--- unixfile.c
+++ unixfile.c
@@ -1,7 +1,8 @@
-
+#include <glob.h>
 #include <stdio.h>
 #include <sys/stat.h>
 #include <utime.h>
+#include <string.h>
 
 #include "fileio.h"
 
@@ -47,7 +48,32 @@ int validate_device_choice(const char *choice)
     return 1;
 }
 
+
 int get_matching_files(uint8_t *result, uint16_t result_size, int argc, char **argv)
 {
-    return 0;
+    int count = 0;
+    uint16_t remaining_size = result_size;
+    for (int i = 0; i < argc; i++) {
+        glob_t globbuf;
+        glob(argv[i], GLOB_NOSORT, NULL, &globbuf);
+        for (int k = 0; k < globbuf.gl_pathc; ++k)
+        {
+            uint8_t pathlen = strlen(globbuf.gl_pathv[k]);
+            uint8_t required_size = 1/*pathlen*/ + 1/*0xff*/ + pathlen;
+            if (required_size >= remaining_size)
+            {
+                fprintf(stderr, "Error: Too many filenames (not enough room in filename buffer).\r\n");
+                return count;
+            }
+            *result++ = pathlen;
+
+            strncpy((char *) result, globbuf.gl_pathv[k], pathlen);
+            result += pathlen;
+            count++;
+
+        }
+        globfree(&globbuf);
+    }
+    *result = 0xFF;
+    return count;
 }
